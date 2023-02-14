3a13c5861225a0cd5bb86d1867dfce31dd98a7bf
肚子里的巨龙 <1670698657@qq.com>
2023-02-14 15:43:49 +0800

    '项目初始化,调整目录结构'

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,29 @@
 <template>
-  <div id="app">
-    <nav>
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </nav>
-    <router-view/>
-  </div>
+    <div id="app">
+       <h1>头条</h1>
+      <router-view/>
+    </div>
 </template>
-
-<style lang="less">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+<script>
+export default {
+  name: 'App',
+   data:function() {
+      return {
+      }
+   },
+//    组件
+  components:{
+      },
+//    方法
+  methods:{
+      },
+//    计算属性
+  computed:{
+      },
+//    过滤器
+  filters:{
+      },
 }
-
-nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
+</script>
+<style>
 </style>
